2f671e72b1b867aaef4a897d67cdf28e9f1d6aed
swdrana <tctr2s@gmail.com>
2022-02-03 19:30:38 +0600

    1+2+3+...+n using recursive function

---
+++ Module-22.5/02-Advance-Problem/00.RecursiveFunction.js
@@ -0,0 +1,14 @@
+/* ০. রিকার্সন বুঝলে চমৎকার, আর না বুঝলে আপাতত রিকার্সন না বুঝলে সেটাকে সাইডে রেখে দাও।
+শুধু মাথায় রাখো রিকার্সন নামে একটা জিনিস আছে।
+সেটা একটা ফাংশনকে নিজের ভিতর থেকে কল করে।  */
+//Sum of this: 1+2+3+....+n using Recursive Function 
+const n=5;
+function sumOfTheSeries(num){
+    if(num>1){
+        return num+sumOfTheSeries(num-1);
+    }else{
+        return num;
+    }
+}
+const total=sumOfTheSeries(n);
+console.log(total);
