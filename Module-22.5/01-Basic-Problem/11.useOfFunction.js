cd28e51b2b754c726f4fb3761428913bc1df81c8
swdrana <tctr2s@gmail.com>
2022-02-03 18:37:36 +0600

    multiplication using function

---
+++ Module-22.5/01-Basic-Problem/11.useOfFunction.js
@@ -0,0 +1,10 @@
+/* ১১. তিনটা সংখ্যার গুনফল বের করে ফাইনাল রেজাল্ট আউটপুট হিসেবে রিটার্ন করতে হবে।
+তুমি কি সেই কোড লিখতে পারবে। যদি পারো তাহলে সেই কোড লিখে ফেলো।  */
+let num1=2;
+var num2=6;
+var num3=4;
+function returnMult(no1,no2,no3){
+    return no1*no2*no3;
+}
+const mul = returnMult(num1, num2, num3);
+console.log(mul);
