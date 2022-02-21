1e302f829ac26be0d976d8b14c47727e50b555cd
swdrana <tctr2s@gmail.com>
2022-02-21 16:04:34 +0600

    template string and object

---
+++ Module-32.5/02.template-string.js
@@ -0,0 +1,13 @@
+/* ২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল 
+এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের 
+অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও।  */
+let day = 'friday';
+let templateString = `today is ${day}`;
+console.log(templateString);
+
+let myObject = {
+    day:'Monday',
+    date: 4
+}
+templateString = `today is ${myObject.day} and date is ${myObject.date}.`;
+console.log(templateString);
